6b355d4b3c11db5bca455d13eda7332e12d80042
daksheshsharma2409 <daksheshsharma2007@gmail.com>
2025-11-17 11:07:37 +0530

    Final Changes

--- js/about.js
+++ js/about.js
@@ -2,20 +2,13 @@ let html_per = 60;
 let css_per = 37.2;
 let js_per = 2.8;
 
-document.getElementById("html-bar").style.backgroundColor = "orange";
 document.getElementById("html-bar").style.width = `${html_per}%`;
-document.getElementById("html-bar").style.borderRadius = "100px 0 0 100px";
-
-document.getElementById("css-bar").style.backgroundColor = "purple";
 document.getElementById("css-bar").style.width = `${css_per}%`;
-
-document.getElementById("js-bar").style.backgroundColor = "yellow";
 document.getElementById("js-bar").style.width = `${js_per}%`;
-document.getElementById("js-bar").style.borderRadius = "0 100px 100px 0";
 
-document.getElementById("html-color").style.backgroundColor = "orange";
-document.getElementById("css-color").style.backgroundColor = "purple";
-document.getElementById("js-color").style.backgroundColor = "yellow";
+document.getElementById("html-color").style.backgroundColor = "#E44D26"; // HTML Orange
+document.getElementById("css-color").style.backgroundColor = "#1572B6"; // CSS Blue
+document.getElementById("js-color").style.backgroundColor = "#F7DF1E"; // JS Yellow
 
 document.getElementById("html-text").innerText = `HTML : ${html_per}%`;
 document.getElementById("css-text").innerText = `CSS : ${css_per}%`;
