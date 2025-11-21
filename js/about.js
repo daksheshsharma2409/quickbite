087947b665b3dbec21bdd1c3335e453748bcd09a
daksheshsharma2409 <daksheshsharma2007@gmail.com>
2025-11-21 14:55:02 +0530

    Minor Changes

--- js/about.js
+++ js/about.js
@@ -6,9 +6,9 @@ document.getElementById("html-bar").style.width = `${html_per}%`;
 document.getElementById("css-bar").style.width = `${css_per}%`;
 document.getElementById("js-bar").style.width = `${js_per}%`;
 
-document.getElementById("html-color").style.backgroundColor = "#E44D26"; // HTML Orange
-document.getElementById("css-color").style.backgroundColor = "#1572B6"; // CSS Blue
-document.getElementById("js-color").style.backgroundColor = "#F7DF1E"; // JS Yellow
+document.getElementById("html-color").style.backgroundColor = "#E44D26";
+document.getElementById("css-color").style.backgroundColor = "#1572B6";
+document.getElementById("js-color").style.backgroundColor = "#F7DF1E";
 
 document.getElementById("html-text").innerText = `HTML : ${html_per}%`;
 document.getElementById("css-text").innerText = `CSS : ${css_per}%`;
